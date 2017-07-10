a0c98d9c04cab031f656eb0193175a652dde18fd
Wendy Fu <comloo99@gmail.com>
2017-07-10 10:22:55 -0700

    changed setup_requires to install_requires

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     	],
     },
     packages=find_packages(),
-    setup_requires=['pytest-runner', 'cairosvg', 'python-dateutil'],
+    install_requires=['pytest-runner', 'cairosvg', 'python-dateutil'],
     tests_require=['pytest']
 )
